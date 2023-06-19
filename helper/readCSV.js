1a67ab9d6d07498decad2fc7ac461efb91d46bd7
Umair Mustafa <umairmustafa753@gmail.com>
2023-06-20 03:06:24 +0500

    change path

--- helper/readCSV.js
+++ helper/readCSV.js
@@ -1,5 +1,5 @@
-const csv = require("csv-parser");
-const fs = require("fs");
+const csv = require('csv-parser');
+const fs = require('fs');
 
 export function readCSV(filePath) {
   return new Promise((resolve, reject) => {
@@ -7,8 +7,8 @@ export function readCSV(filePath) {
 
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on("data", (data) => results.push(data))
-      .on("end", () => resolve(results))
-      .on("error", (error) => reject(error));
+      .on('data', (data) => results.push(data))
+      .on('end', () => resolve(results))
+      .on('error', (error) => reject(error));
   });
 }
