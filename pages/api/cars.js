e847056fd43aa98d68852eda545e396c5fc9b88c
Umair Mustafa <umairmustafa753@gmail.com>
2023-06-20 00:51:04 +0500

    added cars api and next structure

---
+++ pages/api/cars.js
@@ -0,0 +1,20 @@
+import { readCSV } from "../../helper/readCSV";
+
+export default async function handler(req, res) {
+  const { carName } = req.query;
+
+  try {
+    const cars = await readCSV("cars.csv");
+
+    const matchingCars = cars.filter((car) =>
+      car.CarName.toLowerCase().includes(carName.toLowerCase())
+    );
+
+    res.status(200).json(matchingCars);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while retrieving car data." });
+  }
+}
